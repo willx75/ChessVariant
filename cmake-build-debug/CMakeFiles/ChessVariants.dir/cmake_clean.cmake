a078bd10f4db7ee76857cd2915f89ce8cbe732ae
NJANGUI wilfried <njangui@informatique.univ-paris-diderot.fr>
2019-01-05 20:47:18 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/ChessVariants.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessVariants.dir/main.cpp.obj"
+  "ChessVariants.pdb"
+  "ChessVariants.exe"
+  "ChessVariants.exe.manifest"
+  "libChessVariants.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessVariants.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
